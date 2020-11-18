1fdbe00f961b384acc8178e5bade238899af9011
Adrian Neagu <adrian_neagu7@yahoo.com>
2020-11-18 22:06:23 +0200

    Modified code to count the questions and score

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 // console.log(choices);
 
 let currentQuestion = {};
@@ -52,6 +54,8 @@ getNewQuestion = () => {
         return window.location.assign("/end.html");
     } 
     questionCounter++;
+    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -75,7 +79,9 @@ choices.forEach(choice => {
        
        const classToApply =
         selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
-        
+        if (classToApply==='correct') {
+            incrementScore(CORRECT_BONUS);
+        }
        selectedChoice.parentElement.classList.add(classToApply);
        
        setTimeout( () => {
@@ -84,5 +90,8 @@ choices.forEach(choice => {
        }, 1000);
     });
 });
-
+incrementScore = num => {
+    score+=num;
+    scoreText.innerText=score;
+}
 startGame();
